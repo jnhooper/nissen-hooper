51448788e175d6e7b8143117b4cba430391e062e
John Hooper <nisse038@gmail.com>
2015-05-01 00:19:45 -0500

    made smarter puppies

--- public/scripts/offended/offended.js
+++ public/scripts/offended/offended.js
@@ -1,19 +1,23 @@
-var OffendedApp=angular.module("OffendedApp",[]);
-OffendedApp.controller('mainCtrl', function($scope){
-    $scope.puppy=function(txt){
-        if($scope.offensive && $scope.text) {
+var OffendedApp = angular.module("OffendedApp", []);
+OffendedApp.controller('mainCtrl', function ($scope) {
+    $scope.puppy = function (txt) {
+        if ($scope.offensive && $scope.text) {
             var offensive = $scope.offensive.split("\n");
-            _.each(offensive,function(word){
-                if(word!="" &&word!=" "){
-                var re = new RegExp(word, 'g');
-                txt = txt.replace(re, 'puppies');
+            _.each(offensive, function (word) {
+                if (word != "" && word != " ") {
+                    var re = new RegExp(word, 'g');
+                    var sub = "puppy";
+                    if (word[word.length - 1] == "s" || word[word.length - 1] == "S") {
+                        sub = "puppies"
+                    }
+                    txt = txt.replace(re, sub);
                 }
             });
         }
         return txt
     };
-    $scope.hover=false;
-    $scope.display=function(){
+    $scope.hover = false;
+    $scope.display = function () {
         $scope.hover = !$scope.hover;
     }
 });
